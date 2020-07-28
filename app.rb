bd871050af317a0be7ecda019239fbcf175c8174
Zachary Lynch <zachary.lynch1337@gmail.com>
2020-07-28 05:14:53 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -5,4 +5,11 @@ class App < Sinatra::Base
   get '/name' do
     "My name is Zek"
   end
+  
+  get '/hometown' do
+    "My home town is Henderson"
+  end
+  
+  get '/favorite-song' do 
+    "my favorite song is All Star"
 end
